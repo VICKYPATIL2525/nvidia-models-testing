4435ef0fc2f80a84262614c6764ed0f94112cf5c
VICKY PATIL <vickyvpatil25@gmail.com>
2026-05-26 16:41:14 +0530

    file restructuring

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello, world!');
-
-// Add event listener to button
-const button = document.getElementById('button');
-button.addEventListener('click', () => {
-	console.log('Button clicked!');
-});
